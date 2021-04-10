59320709de99cb9a80611bb0e27195310921c206
noboru-i <ishikura.noboru@gmail.com>
2021-04-10 16:36:02 +0900

    switch sampler preset/bank

--- AudioKitSamples/PlayMidiWithSf2/PlayMidiWithSf2View.swift
+++ AudioKitSamples/PlayMidiWithSf2/PlayMidiWithSf2View.swift
@@ -8,25 +8,68 @@
 import SwiftUI
 import AudioKit
 
+enum BankPreset {
+    case melodic7
+    case melodic12
+    case percussion0
+
+    var preset: Int {
+        switch self {
+        case .melodic7:
+            return 7
+        case .melodic12:
+            return 12
+        case .percussion0:
+            return 0
+        }
+    }
+    var bank: Int {
+        switch self {
+        case .melodic7:
+            return kAUSampler_DefaultBankLSB
+        case .melodic12:
+            return kAUSampler_DefaultBankLSB
+        case .percussion0:
+            return 128
+        }
+    }
+}
+
 struct PlayMidiWithSf2View: View {
     var controller = PlayMidiController()
 
     @State private var noteNumber: Double = 60
+    @State private var selectedBankPreset = BankPreset.melodic7
 
     var body: some View {
         VStack {
-            Text("Play MIDI. Note Number: \(String(format: "%.0f", noteNumber))")
+            Picker("Bank, Preset",
+                   selection: $selectedBankPreset) {
+                Text("Melodic 7").tag(BankPreset.melodic7)
+                Text("Melodic 12").tag(BankPreset.melodic12)
+                Text("Percussion 0").tag(BankPreset.percussion0)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .onChange(of: selectedBankPreset, perform: { value in
+                controller.changeSampler(preset: value.preset, bank: value.bank)
+            })
+            Spacer()
+                .frame(height: 50)
             Slider(
                 value: $noteNumber,
                 in: 0...127,
                 onEditingChanged: {_ in }
             )
-            Button(action: playNote) {
-                Text("Play note")
-            }
-        }.onAppear {
+            Text("Note Number: \(String(format: "%.0f", noteNumber))")
+            Spacer()
+                .frame(height: 50)
+            Button("Play note", action: playNote)
+        }
+        .padding()
+        .onAppear {
             controller.setup()
-        }.onDisappear {
+        }
+        .onDisappear {
             controller.dispose()
         }
     }
@@ -70,6 +113,15 @@ class PlayMidiController {
             try AKManager.stop()
         } catch let error {
             print("AKManager.stop error. \(error)")
+            return
+        }
+    }
+
+    func changeSampler(preset: Int, bank: Int) {
+        do {
+            try tapSampler.loadSoundFont(sf2FilePath, preset: preset, bank: bank)
+        } catch let error {
+            print("Failed to changeSampler. \(error.localizedDescription)")
             return
         }
     }
