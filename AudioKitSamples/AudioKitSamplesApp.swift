ce1a8779b494a2358ad71e97eb1450a4f8bdfcfa
noboru-i <ishikura.noboru@gmail.com>
2021-04-10 13:55:28 +0900

    Initial Commit

---
+++ AudioKitSamples/AudioKitSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKitSamplesApp.swift
+//  AudioKitSamples
+//
+//  Created by 石倉昇 on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct AudioKitSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioKitSamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AudioKitSamples
+//
+//  Created by 石倉昇 on 2021/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
